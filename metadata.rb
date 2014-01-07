f5c36487f053e19da97eab3e1397afcaaffbc68b
Yusuke Tanaka <y.tanaka@aktsk.jp>
2014-01-07 11:43:36 +0900

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'csouls@gmail.com'
 license          'MIT'
 description      'Installs pssh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "pssh::default",
   "Installs the pssh."
