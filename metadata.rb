12185a6027774b0806112b35dc92b9ee84fc1dba
Yusuke Tanaka <y.tanaka@aktsk.jp>
2014-01-07 11:40:05 +0900

    Update License

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'pssh'
 maintainer       'Akatsuki Inc.'
 maintainer_email 'csouls@gmail.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs pssh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
